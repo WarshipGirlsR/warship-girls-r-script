caa32fd491ba7ac8e88cc1a21930e638b87e80d3
WarshipGirlsR <WarshipGirlsR>
2017-05-04 20:35:18 +0800

    update Co and Promise

--- EventQuery.lua
+++ EventQuery.lua
@@ -1,10 +1,14 @@
 local sz
 local socket
 
+-- get the time
 local gettimeFunc = function()
   return os.time() * 1000
 end
--- for touchsprite
+-- sleep
+local mSleep = mSleep
+
+-- for touchsprite socket
 local _ = (function()
   local pcallRes = pcall(function()
     sz = require 'sz'
@@ -12,12 +16,18 @@ local _ = (function()
     gettimeFunc = function()
       return socket.gettime() * 1000
     end
+    mSleep = mSleep or function(n)
+      socket.select(nil, nil, n / 1000)
+    end
   end)
   if (pcallRes) then return end
   local pcallRes2 = pcall(function()
     socket = require 'socket'
     gettimeFunc = function()
       return socket.gettime() * 1000
+    end
+    mSleep = mSleep or function(n)
+      socket.select(nil, nil, n / 1000)
     end
   end)
 end)()
@@ -44,21 +54,33 @@ local getEventId = (function()
   end
 end)()
 
+-- 主队列
+local eventQuery = {}
+-- 各个小队列
 local screenListenerQuery = {}
 local buttonListenerQuery = {}
 local timerQuery = {}
 
-function getEventObj(func, time, isInterval)
+function getEventObj(func, time, isInterval, ms)
   return {
-    id = getEventId(),
-    time = time,
+    id = getEventId() or 0,
+    time = time or 0,
     func = func,
-    isInterval = isInterval
+    isInterval = isInterval or false,
+    ms = ms or 0
   }
+end
+
+function setimmediate(func)
+  if (type(func) ~= 'function') then return 0 end
+  local eventObj = getEventObj(func)
+  table.insert(eventQuery, eventObj)
+  return eventObj.id
 end
 
 function setTimeout(func, ms)
   if (type(func) ~= 'function') then return 0 end
+  if ((type(ms) ~= 'number') or (ms < 4)) then ms = 4 end
   local time = gettimeFunc() + ms
   local eventObj = getEventObj(func, time)
   table.insert(timerQuery, eventObj)
@@ -67,40 +89,94 @@ end
 
 function setInterval(func, ms)
   if (type(func) ~= 'function') then return 0 end
+  if ((type(ms) ~= 'number') or (ms < 4)) then ms = 4 end
   local time = gettimeFunc() + ms
-  local eventObj = getEventObj(func, time, true)
+  local eventObj = getEventObj(func, time, true, ms)
   table.insert(timerQuery, eventObj)
   return eventObj.id
 end
 
 function run()
   local continue = 0
-  local thisTime = gettimeFunc()
+  local thisTime = 0
+  local sleepTime = 0
+
   repeat
     continue = 0
-    while (#timerQuery > 0) do
+    sleepTime = 0
+
+    -- run eventQuery
+    for key, value in ipairs(eventQuery) do
       continue = continue + 1
-      local value = timerQuery { 1 }
-      if (value.time < thisTime) then
-        table.remove(timerQuery, pos)
-        value.func()
+      value.func()
+      -- setInterval event
+    end
+    eventQuery = {}
+
+
+    -- read event from other Query
+    thisTime = gettimeFunc()
+
+    -- timeQuery
+    local newTimeQuery = {}
+    for key, value in ipairs(timerQuery) do
+      continue = continue + 1
+      if (value.time <= thisTime) then
+        table.insert(eventQuery, value)
+
+        -- setInterval event
+        if (value.isInterval) then
+          repeat
+            value.time = value.time + value.ms
+          until (value.time > thisTime)
+          sleepTime = math.min(sleepTime, value.time)
+          table.insert(newTimeQuery, value)
+        end
+
+        console.log(value)
+      else
+        sleepTime = math.min(sleepTime, value.time)
+        table.insert(newTimeQuery, value)
       end
     end
-    for key, value in iparis(screenListenerQuery) do
+    timerQuery = newTimeQuery
+    newTimeQuery = nil
+
+    -- screenListenerQuery
+    local newScreenListenerQuery = {}
+    for key, value in ipairs(screenListenerQuery) do
       continue = continue + 1
       if (value.time < thisTime) then
-        value.func()
+        table.insert(eventQuery, value)
+      else
+        table.insert(newScreenListenerQuery, value)
       end
     end
-    for key, value in iparis(buttonListenerQuery) do
+    screenListenerQuery = newScreenListenerQuery
+    newScreenListenerQuery = nil
+
+    -- buttonListenerQuery
+    local newButtonListenerQuery = {}
+    for key, value in ipairs(buttonListenerQuery) do
       continue = continue + 1
       if (value.time < thisTime) then
-        value.func()
+        table.insert(eventQuery, value)
+      else
+        table.insert(newButtonListenerQuery, value)
       end
+    end
+    buttonListenerQuery = newButtonListenerQuery
+    newButtonListenerQuery = nil
+
+    if (#eventQuery <= 0) then
+      mSleep(sleepTime)
     end
   until (continue <= 0)
 end
 
 return {
+  setimmediate = setimmediate,
   setTimeout = setTimeout,
+  setInterval = setInterval,
+  run = run,
 };
--- test.lua
+++ test.lua
@@ -1,3 +1,54 @@
 local co = require 'Co'
+local Promise = require 'Promise'
 require 'console'
 local eq = require 'EventQuery'
+
+local runQ = {
+  queue = {},
+}
+function runQ:add(value)
+  table.insert(self.queue, value)
+end
+
+function runQ:run()
+  local value = table.remove(self.queue, 1)
+  if (type(value) == 'function') then
+    value()
+    return true
+  end
+  return false
+end
+
+local getNewPromise = function(text)
+  return Promise.new(function(resolve, reject)
+    runQ:add(function()
+      resolve(text)
+    end)
+  end)
+end
+
+co(coroutine.create(function()
+  console.log('start a')
+  for i = 1, 3, 1 do
+    coroutine.yield(getNewPromise(500))
+    console.log('a' .. i)
+  end
+  console.log('end a')
+end)).catch(function(err)
+  console.log(err)
+end)
+
+co(coroutine.create(function()
+  console.log('start b')
+  for i = 1, 3, 1 do
+    coroutine.yield(getNewPromise(1000))
+    console.log('b' .. i)
+  end
+  console.log('end b')
+end)).catch(function(err)
+  console.log(err)
+end)
+
+
+while (runQ:run()) do
+end
